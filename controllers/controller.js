6b15dbefac7ef2389414aeee7eefff78482fd8bd
dmeekins <drew.a.meekins@gmail.com>
2022-05-03 15:18:31 -0400

    characters are all showing on page

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,42 @@
 // set up the requirements for express
 const express = require('express')
+const res = require('express/lib/response')
 const router = express.Router()
 const Character = require('../models/character-model')
 const app = express()
 
 //setup routes
 router.get('/', (req, res) => {
-    res.send('characters')
+    res.render('welcome')
 })
 
+router.get('/startgame', (req, res) => {
+    res.render('startGame')
+})
+
+router.get('/character', (req, res) => {
+    Character.find({})
+    .then(character => {
+        res.render('index', {character})
+    })
+})
+
+//find characters from db
+router.get('/character/:id', (req, res) => {
+    Character.findById(req.params.id)
+    .then(character => {
+        res.render('index', {character})
+    })
+})
+
+router.get('/character/:id', (req, res) => {
+    Character.find({})
+    .then(character => {
+        res.json(character[req.params.id])
+    })
+})
+
+//cycle through characters
 
 
 module.exports = router
